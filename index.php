8b273137261728563408b9bb85b911d466bd2f06
pgbmjc <pgbmjc@gmail.com>
2023-04-29 01:46:09 -0400

    teste

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" type="text/css" href="css/index.css">
+		<title>Rental World</title>
+	</head>
+
+	
+    <body>
+		<div class="teste_menu">
+			<?php include 'menu.html';?>
+		</div>
+			
+    </body>
+
+
+</html>
