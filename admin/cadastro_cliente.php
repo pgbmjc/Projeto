214a903329b6c01828ba805d15812dd8cd552caf
pgbmjc <pgbmjc@gmail.com>
2023-05-02 20:13:54 -0400

    altercao cadastro cliente

--- admin/cadastro_cliente.php
+++ admin/cadastro_cliente.php
@@ -17,26 +17,60 @@
 	<main>
 		
 		<form id="cadastro" name="cadastro_cliente" class="form_cadastro" method="post" action="conexao.php">
-			<p>Cadastro de Cliente<br><br></p>
 			<div class="cadastro_cliente_geral">
 				<div class="cadastro_cliente1">
+					<p>Cadastro de Cliente<br></p>
 					<div>
 						<legend>Nome Completo</legend>
 						<input class="cadastro_cliente_input" type="text" placeholder="Maria da Silva e Silva" id="nome" nome="nome" required autofocus>
 					</div>
 		
 					<div>
-					<legend>Celular</legend>
-					<input class="cadastro_cliente_input" type="tel" placeholder="99 99999-9999" pattern="[0-9]{2}-[0-9]{5}-[0-9]{4}" id="celular" nome="celular" required autofocus>
+						<legend>Celular</legend>
+						<input class="cadastro_cliente_input" type="tel" placeholder="99 99999-9999" pattern="[0-9]{2}-[0-9]{5}-[0-9]{4}" id="celular" nome="celular" required autofocus>
 					</div>
 
 					<div>
 						<legend>CPF</legend>
 						<input class="cadastro_cliente_input" type="number" maxlength="11" minlegth="11" placeholder="000.000.000-00" id="cpf" nome="cpf" required autofocus>
 					</div>
+
+					<div>
+						<legend>Usuario do Cliente</legend>
+						<input class="cadastro_cliente_input" type="text" maxlength="11" minlegth="11" placeholder="usuario" id="cpf" nome="cpf" required autofocus>
+					</div>
+
+					<div>
+						<legend>Senha do cliente</legend>
+						<input class="cadastro_cliente_input" type="teste" maxlength="11" minlegth="11" placeholder="senha" id="cpf" nome="cpf" required autofocus>
+					</div>
+					
+					<div>
+						<legend>Sexo:</legend>
+						<select class="cadastro_cliente_input" name="tipo_sexo" id="tipo_sexo">
+							<option value="masculino">Masculino</option>
+							<option value="feminino">Feminino</option>
+							<option value="mutante">Mutante</option>
+						</select>
+					</div>
 				</div>
-			
+
 				<div class="cadastro_cliente1">
+					<p>Cadastro de Funcionarios<br></p>
+					<div>
+						<legend>Nome Completo</legend>
+						<input class="cadastro_cliente_input" type="text" placeholder="Maria da Silva e Silva" id="nome" nome="nome" required autofocus>
+					</div>
+					<div>
+						<legend>Celular</legend>
+						<input class="cadastro_cliente_input" type="tel" placeholder="99 99999-9999" pattern="[0-9]{2}-[0-9]{5}-[0-9]{4}" id="celular" nome="celular" required autofocus>
+					</div>
+
+					<div>
+						<legend>CPF</legend>
+						<input class="cadastro_cliente_input" type="number" maxlength="11" minlegth="11" placeholder="000.000.000-00" id="cpf" nome="cpf" required autofocus>
+					</div>
+						
 					<div>
 						<legend>Usuario</legend>
 						<input class="cadastro_cliente_input" type="text" placeholder="000000000000000" id="usuario" nome="usuario" required autofocus>
@@ -46,6 +80,13 @@
 						<legend>Senha</legend>
 						<input class="cadastro_cliente_input" type="text" placeholder="000000000000000" id="senha" nome="senha" required autofocus>
 					</div>
+
+					<div>
+						<legend>Tipo de usuario</legend>
+						<input class="cadastro_cliente_input" type="text" placeholder="1-administrador / 2-usuario padrao" id="senha" nome="senha" required autofocus>
+					</div>
+
+
 
 					<div>
 						<legend>Sexo:</legend>
