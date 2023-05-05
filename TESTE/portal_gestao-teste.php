1d484e185474f6472262bd6c73f92d5109c40cd5
pgbmjc <pgbmjc@gmail.com>
2023-05-05 17:59:26 -0400

    novo_menu1

---
+++ TESTE/portal_gestao-teste.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" type="text/css" href="portal_gestao.css">
+		<title>Rental World</title>
+	</head>
+
+<body>
+	<header>
+    	 <div>
+			<?php include 'menu_gestao_lateral.html';?>
+		</div>
+	</header>
+
+	<main>
+		<div class="conteudo_portal">
+			<div class="conteudo">
+				<p> Rental World System <br></p>
+				<span> Usuario: Paulo Giovani<br>Data:<br>Hora:<br></span>
+			</div>
+		
+			<div class="conteudo">
+				<img src="../img/frota.png" class="img_central">
+			</div>
+		</div>
+	</main>
+
+	<footer>
+       	<div>
+			<?php include 'rodape_gestao.html';?>
+		</div>
+	</footer>
+
+</body>
+</html>
