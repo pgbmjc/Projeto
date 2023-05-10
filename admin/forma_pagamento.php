37d364a09297ae86fa48ecef9e46de9897b34e5b
pgbmjc <pgbmjc@gmail.com>
2023-05-09 21:15:14 -0400

    ajustes

--- admin/forma_pagamento.php
+++ admin/forma_pagamento.php
@@ -19,22 +19,22 @@
 			<h2> Formas de pagamentos </h2><br>
 			<div class="cadastro_div">
 				
-				<div class=cadastro_div_campo>
+				<div class=cadastro_div_codigo>
 					<label>Código</label>
 					<input class="input_cadastro" type="number" placeholder="000" id="cod_pagamento" nome="cod_pagamento" required autofocus>
 				</div>
 
-				<div class=cadastro_div_campo>
+				<div class=cadastro_div_descricao>
 					<label>Tipo de pagamento</label>
 					<input class="input_cadastro" type="text" placeholder="cartão, debito, dinheiro, etc" id="forma_pagamento" nome="forma_pagamento" required autofocus>
 				</div>
-
+				
+			</div>
 				<div class="botoes">
                     <input class="botao" type="submit" id="btn_buscar" name="btn_buscar" value="Buscar">
                     <input class="botao" type="submit" id="btn_salvar" name="btn_salvar" value="Incluir">
                     <input class="botao" type="reset" value="Limpar">
-                </div>
-			</div>
+            	</div>
 		</form>
 	</main>
 
@@ -56,10 +56,25 @@
 					</tr>
 					<tr>
 						<td>02</td>
-						<td>Cartão</td>
+						<td>Cartão de Credito</td>
 						<td><input type="image" name="editar_table" id="btn_editar" src="../img/editar.png" onclick=""></td>
 						<td><input type="image" name="delete_table" id="btn_delete" src="../img/lixeira.png" onclick=""></td>
 					</tr>
+					<tr>
+						<td>03</td>
+						<td>Cartão de Debito</td>
+						<td><input type="image" name="editar_table" id="btn_editar" src="../img/editar.png" onclick=""></td>
+						<td><input type="image" name="delete_table" id="btn_delete" src="../img/lixeira.png" onclick=""></td>
+					</tr>
+
+					<tr>
+						<td>04</td>
+						<td>Pix</td>
+						<td><input type="image" name="editar_table" id="btn_editar" src="../img/editar.png" onclick=""></td>
+						<td><input type="image" name="delete_table" id="btn_delete" src="../img/lixeira.png" onclick=""></td>
+					</tr>
+
+
 				</tbody>
 		</table>
 		
