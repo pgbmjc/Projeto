04892d5149a6aa405b4ed9331bfe576cc52ae9ee
pgbmjc <pgbmjc@gmail.com>
2023-06-05 21:40:02 -0400

    Adjust local

--- conexao.php
+++ conexao.php
@@ -10,13 +10,11 @@
 	
 	date_default_timezone_set("America/Manaus");
 	
-	$host_ip = $_SERVER['HTTP_HOST'];
+	$host_ip = 'rentalworld.herokuapp.com';
 	
 	$url = "http://".$host_ip."/projeto";
 
 	$url_admin = "http://".$host_ip."/projeto/admin";
-
-	$url_aluno = "http://".$host_ip."/projeto";
 
 	$url_login_gestao = "http://".$host_ip."/projeto/login_gestao.php";
 	
